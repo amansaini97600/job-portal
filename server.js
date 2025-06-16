ae93cfdd59406a59d6b687af4778e89e2bb89959
amansaini97600 <amansaini9700@gmail.com>
2025-06-16 16:59:18 +0530

    fixed some problem

--- server.js
+++ server.js
@@ -226,17 +226,7 @@ app.post("/api/google-login", async (req, res) => {
   }
 });
 
-// GET /api/user/:id
-Router.get("/user/:id", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id).select("-password");
-    if (!user) return res.status(404).json({ success: false, message: "User not found" });
 
-    res.json({ success: true, user });
-  } catch (err) {
-    res.status(500).json({ success: false, message: "Server error" });
-  }
-});
 
 
 
